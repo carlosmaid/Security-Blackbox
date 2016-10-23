c350f013556664b5fe6808237b3de538b4836912
carlosmaid@hotmail.com <carlosmaid@hotmail.com>
2016-10-23 09:59:49 -0300

    Nuevo codigo mas reducido y limpo, ya funciona con una baliza comun

--- core.cs
+++ core.cs
@@ -1,178 +1,88 @@
-ï»¿using System;
-using System.Collections.Generic;
-using System.Text;
 using Sandbox.ModAPI;
-using Sandbox.ModAPI.Interfaces;
-using Sandbox.Common;
-// using Sandbox.Common.components;
-using Sandbox.Common.ObjectBuilders;
-using Sandbox.Common.ObjectBuilders.Definitions;
-using Sandbox.Game;
-using Sandbox.Game.Entities;
-using Sandbox.Game.Entities.Blocks;
-using Sandbox.Game.Entities.Character;
-using Sandbox.Game.World;
-using Sandbox.Definitions;
-using Sandbox.Engine;
-using SpaceEngineers.ObjectBuilders;
-using SpaceEngineers.ObjectBuilders.Definitions;
-using SpaceEngineers;
-using SpaceEngineers.Game;
-using VRage;
-using VRage.Game;
-using VRage.Game.Entity;
 using VRage.Game.Components;
-using VRage.Game.ModAPI;
 using VRage.ModAPI;
-using VRage.Game.ObjectBuilders;
 using VRage.ObjectBuilders;
-using SpaceEngineers.Game.ModAPI;
-using VRageMath;
+using System.Collections.Generic;
+using VRage.Game.ModAPI;
+using System.Text.RegularExpressions;
+using VRage.Game;
 
-
-
-namespace spacelatino
+[MyEntityComponentDescriptor(typeof(MyObjectBuilder_CubeGrid))]
+public class RenameCubeGrid : MyGameLogicComponent
 {
-    
+    private MyObjectBuilder_EntityBase builder;
 
+    public override void Init(MyObjectBuilder_EntityBase objectBuilder)
+    {
+        builder = objectBuilder;
+        Entity.NeedsUpdate |= MyEntityUpdateEnum.BEFORE_NEXT_FRAME;
+    }
 
-    [MyEntityComponentDescriptor(typeof(MyObjectBuilder_Beacon))]
-    public class Exchangerlogic : MyGameLogicComponent
-        {
-        VRage.ObjectBuilders.MyObjectBuilder_EntityBase m_objectBuilder = null;
-        private bool m_greeted = false;
-        IMyFunctionalBlock m_block;
-        public long OwnerId { get { return m_block.OwnerId; } }
-        public bool IsBeaconSecurity { get; private set; }
- 
-  
+    public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
+    {
+        return copy ? builder.Clone() as MyObjectBuilder_EntityBase : builder;
+    }
+
+    public override void UpdateOnceBeforeFrame()
+    {
+        base.UpdateOnceBeforeFrame();
+        ((IMyCubeGrid)Entity).OnBlockAdded += RenameCubeGrid.OnBlockAdded;
+    }
+
+    public static void OnBlockAdded(IMySlimBlock block)
+    {
+
+        IMyCubeGrid grid = block.CubeGrid as IMyCubeGrid;
+        if (grid == null)
+            return;
+
         
-        public override void Close()
+        // On récupère la position du block
+        VRageMath.Vector3D position;
+        block.ComputeWorldCenter(out position);
+            
+        // Création de la sphère
+        VRageMath.BoundingSphereD sphere = new VRageMath.BoundingSphereD(position, 15);
+            
+        // Recherche des joueurs présent dans la sphère
+        List<IMyPlayer> players = new List<IMyPlayer>();
+        MyAPIGateway.Players.GetPlayers(players, p => sphere.Contains(p.GetPosition()) == VRageMath.ContainmentType.Contains);
+
+     IMyGridTerminalSystem gridTerminal = MyAPIGateway.TerminalActionsHelper.GetTerminalSystemForGrid(grid);
+       
+     IMyCubeBlock targetFunctionalBlock = block.FatBlock as IMyCubeBlock;
+
+        foreach (IMyPlayer player in players)
         {
+             List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
+             gridTerminal.GetBlocks(blocks);
+             foreach (var blc in blocks)
+             {
 
-        }
+                  if (blc is IMyBeacon && blc.IsWorking){
+                      
+                 MyRelationsBetweenPlayerAndBlock relation = blc.GetUserRelationToOwner(player.PlayerID); //(player.PlayerID)
 
-        public override void Init(VRage.ObjectBuilders.MyObjectBuilder_EntityBase objectBuilder)
-        {
-            Entity.NeedsUpdate |= MyEntityUpdateEnum.EACH_10TH_FRAME;
-                        
-            m_objectBuilder = objectBuilder;
-            m_block = Entity as IMyFunctionalBlock;
+                      // y comprueba si el bloque esta a nombre del jugador o de la faccion
+                     if (relation != MyRelationsBetweenPlayerAndBlock.Owner && relation != MyRelationsBetweenPlayerAndBlock.FactionShare && relation != MyRelationsBetweenPlayerAndBlock.NoOwnership)
+                     {
+                         MyAPIGateway.Utilities.ShowNotification("cant build over a grid with an active security core, destroy it first", 2000, MyFontEnum.Red);
+                         (grid as IMyCubeGrid).RemoveBlock(block, true);
+                         if (block.FatBlock != null)
+                             block.FatBlock.Close();
+                     
+                     }
 
-        }
-
-        public override void MarkForClose()
-        {
-        }
-
-        public override void UpdateAfterSimulation()
-        {
-        }
-
-        public override void UpdateAfterSimulation10()
-        {
-        }
-
-        public override void UpdateAfterSimulation100()
-        {
-        }
-
-        public override void UpdateBeforeSimulation()
-        {
                   }
 
-        public override void UpdateBeforeSimulation10()
-        {
-            if (MyAPIGateway.Session.Player == null)
-            {
-                return;
-            }
-
-            // si esta a menos de 3 metros
+             }
             
-           if ((MyAPIGateway.Session.Player.GetPosition() - Entity.GetPosition()).Length() < 3f)
-            {
-              // si no fue saludado, tiene energia y esta a nombre de alguien
-               if (!m_greeted && IsPowered && m_block.OwnerId != 0)
-                {
-                   // Itera por todos los players 
-                   List<IMyPlayer> players = new List<IMyPlayer>();
-                MyAPIGateway.Players.GetPlayers(players, x => x.Controller != null && x.Controller.ControlledEntity != null);
-                foreach (IMyPlayer player in players)
-                {
-                    // y comprueba si el beacon esta a nombre del jugador o de la faccion
-                    MyRelationsBetweenPlayerAndBlock relation = m_block.GetUserRelationToOwner(player.PlayerID);
-                    if (relation != MyRelationsBetweenPlayerAndBlock.Owner && relation != MyRelationsBetweenPlayerAndBlock.FactionShare)
-                        continue;
-                    // si el jugador es de la faccion entonces:
-
-                    // muestra un mensaje
-                    MyAPIGateway.Utilities.ShowMissionScreen("Titulo", "subtitulo", "", "Bla bla bla...");
-                    // setea el radio de transmision a 2000 metros
-                    TerminalPropertyExtensions.SetValueFloat((IMyFunctionalBlock)m_block, "Radius", 2000f);
-                    m_greeted = true;
-                }
-             }    
-                   
-            }   
-            
-           
         }
-
-        public override void UpdateBeforeSimulation100()
-        {
-         
-              
-        }
-
-        public override void UpdateOnceBeforeFrame()
-        {
-        }
-
-        public override MyObjectBuilder_EntityBase GetObjectBuilder(bool copy = false)
-        {
-            return m_objectBuilder;
-        }
-
-
-        public bool IsPowered
-        {
-            // esta funcion chequea si un grid tiene una fuente de energia activada
-            // ya sea un reactor, una bateria o un panel solar.
-            get
-            {
-                IMyCubeGrid grid = (IMyCubeGrid)Entity.GetTopMostParent();
-                if (grid == null)
-                    return false;
-
-                IMyGridTerminalSystem gridTerminal = MyAPIGateway.TerminalActionsHelper.GetTerminalSystemForGrid(grid);
-                List<IMyTerminalBlock> blocks = new List<IMyTerminalBlock>();
-                gridTerminal.GetBlocks(blocks);
-                foreach (var block in blocks)
-                {
-                    if (block is IMyReactor)
-                    {
-                        if (block.IsWorking)
-                            return true;
-                    }
-
-                    var battery = block as IMyBatteryBlock;
-                    if (battery != null)
-                    {
-                        if (battery.CurrentStoredPower > 0f && battery.IsWorking)
-                            return true;
-                    }
-
-                    var solar = block as IMySolarPanel;
-                    if (solar != null)
-                    {
-                        if (solar.IsWorking)
-                            return true;
-                    }
-                }
-                return false;
-            }
-           }
-
     }
+
 }
+
+    
+  
+ 
+
